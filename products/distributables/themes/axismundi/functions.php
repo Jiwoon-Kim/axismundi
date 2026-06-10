53cbfff4ff45c09243bf6e328dc4c8e1a8ced1a0
Ji-woon <kimjiwoon75@gmail.com>
2026-06-10 18:36:04 +0900

    Constrain attachment featured artwork to intrinsic size

--- products/distributables/themes/axismundi/functions.php
+++ products/distributables/themes/axismundi/functions.php
@@ -12,7 +12,7 @@
 defined( 'ABSPATH' ) || exit;
 
 if ( ! defined( 'AXISMUNDI_VERSION' ) ) {
-	define( 'AXISMUNDI_VERSION', '0.1.5' );
+	define( 'AXISMUNDI_VERSION', '0.1.6' );
 }
 
 // Theme-internal attachment page renderer. WordPress core/post-content only
